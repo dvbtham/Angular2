7e6d15fc822cc44eb7bc16450bb1c3aa29a3bffd
Thâm Davies <thamdv96@gmail.com>
2017-05-09 00:04:00 -0700

    adding tutorial component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: `<h1>Helo world!</h1>`,
+  template: `<h1>Helo world!</h1> 
+  <my-tut>Loading...</my-tut>`,
 })
 export class AppComponent { }
